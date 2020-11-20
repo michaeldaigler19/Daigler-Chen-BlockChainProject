96c1a98abe688786cd32d98e9855d2a4166d8eed
michaeldaigler19 <70345218+michaeldaigler19@users.noreply.github.com>
2020-11-20 15:55:56 -0500

    Add files via upload

---
+++ src/StakeHolder.java
@@ -0,0 +1,16 @@
+public class StakeHolder {
+    String Id;
+    String Name;
+    String Address;
+    Double Balance;
+
+    public StakeHolder(String id, String name, String address, Double balance) {
+        this.Id = id;
+        this.Name = name;
+        this.Address = address;
+        this.Balance = balance;
+    }
+
+    // Need Setter and Getters for each
+
+}
